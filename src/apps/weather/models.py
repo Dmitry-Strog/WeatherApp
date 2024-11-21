bbf22ad181a6ced275dd9eb246d061e178cae5d5
Dima <strogansev.dima@yandex.ru>
2024-11-22 01:12:51 +0300

    refactor: remove custom id field from weather app model

--- src/apps/weather/models.py
+++ src/apps/weather/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models import IntegerField, CharField, DecimalField
 
-# Create your models here.
+from apps.user.models import CustomUser
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='locations')
+    Latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    Longitude = models.DecimalField(max_digits=9, decimal_places=6)
