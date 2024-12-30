3f0244866e6c6b84e1219baeb18d40c5ec1f0f44
Dima <strogansev.dima@yandex.ru>
2024-12-31 00:17:41 +0300

    feat: add user registration

--- src/apps/user/views.py
+++ src/apps/user/views.py
@@ -1,3 +1,40 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+from apps.user.forms import LoginForm, RegisterForm
+
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['login']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('weather:home'))
+    else:
+        form = LoginForm()
+    context = {'form': form}
+    return render(request, "login.html", context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('weather:home'))
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('weather:home'))
+    else:
+        form = RegisterForm()
+    context = {'form': form}
+    return render(request, "register.html", context)
