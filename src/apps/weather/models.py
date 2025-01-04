97f305326850dbc4a6008c61b954a4a1327a26ec
Dima <strogansev.dima@yandex.ru>
2025-01-04 19:51:01 +0300

    fix: rename latitude,longitude

--- src/apps/weather/models.py
+++ src/apps/weather/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models import IntegerField, CharField, DecimalField
 
 from apps.user.models import CustomUser
 
@@ -7,5 +6,8 @@ from apps.user.models import CustomUser
 class Location(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='locations')
-    Latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    Longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __str__(self):
+        return self.name
